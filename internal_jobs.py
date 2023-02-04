657485290a921d0df23f8821bae2e078ae791091
Mitul <mitulvpatel2211@gmail.com>
2023-02-04 17:18:18 +0530

    python changes commited

--- internal_jobs.py
+++ internal_jobs.py
@@ -5,7 +5,7 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
 
     total_hours = 0
 
-    internal_jobs = {"May": {"task category": [], "task service": [], "date": [], "hours": [] }, "Jun": {"task category": [], "task service": [], "date": [], "hours": [] }, 'Jul': {"task category": '', "task service": '', "date": [], "hours": ''},
+    internal_jobs = {"May": {"task category": [], "task service": [], "date": [], "hours": [] }, "Jun": {"task category": [], "task service": [], "date": [], "hours": [] }, 'Jul': {"task category": [], "task service": [], "date": [], "hours": []},
     "Aug": {"task category": [], "task service": [], "date": [], "hours": [] }, "Sep": {"task category": [], "task service": [], "date": [], "hours": [] }, "Oct" : {"task category": [], "task service": [], "date": [], "hours": [] },
     "Nov": {"task category": [], "task service": [], "date": [], "hours": [] }, "Dec": {"task category": [], "task service": [], "date": [], "hours": [] },
     "Jan": {"task category": [], "task service": [], "date": [], "hours": [] }, "Feb": {"task category": [], "task service": [], "date": [], "hours": []},
@@ -13,6 +13,9 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
     
     internal_jobs = dict(internal_jobs)
     print(type(internal_jobs))
+
+    landscape_presence = 'False'
+    poolservice_presence = 'False'
 
     MAY['Hours'] = [float(i) for i in MAY['Hours']]
     h = (MAY[MAY['Type']=='INTERNAL']['Hours']).sum()
@@ -27,7 +30,9 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = MAY[MAY['Type']=='INTERNAL']['category']
         task_service = MAY[MAY['Type']=='INTERNAL']['service']
         task_hour = MAY[MAY['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['MAY 29', 'MAY 30'] * int(len(task_service)/2)
+        day_1 = ['MAY 30'] * ((len(task_service)) // 2)
+        day_2 = ['MAY 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
        #internal_jobs['May'] = {'task_category': task_category.to_list(), 'task_service': task_service.to_list(), 'date': date_scheduled}
@@ -35,8 +40,7 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         internal_jobs["May"]["task service"].extend(task_service.to_list())
         internal_jobs["May"]["date"].extend(date_scheduled)
         internal_jobs["May"]["hours"].extend(task_hour)
-    else:
-        internal_jobs['May'] = 'no jobs scheduled'
+
 
     JUN['Hours'] = [float(i) for i in JUN['Hours']]
     h = (JUN[JUN['Type']=='INTERNAL']['Hours']).sum()
@@ -53,7 +57,9 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = JUN[JUN['Type']=='INTERNAL']['category']
         task_service = JUN[JUN['Type']=='INTERNAL']['service']
         task_hour = JUN[JUN['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['JUN 29', 'JUN 30'] * int(len(task_service)/2)
+        day_1 = ['JUN 29'] * ((len(task_service)) // 2)
+        day_2 = ['JUN 30'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         #internal_jobs['Jun'] = {'task_category': task_category.to_list(), 'task_service': task_service.to_list(), 'date': date_scheduled}
@@ -61,8 +67,7 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         internal_jobs["Jun"]["task service"].extend(task_service.to_list())
         internal_jobs["Jun"]["date"].extend(date_scheduled)
         internal_jobs["Jun"]["hours"].extend(task_hour)
-    else:
-        internal_jobs['Jun'] = 'no jobs scheduled'
+
 
     JUL['Hours'] = [float(i) for i in JUL['Hours']]
     h = (JUL[JUL['Type']=='INTERNAL']['Hours']).sum()
@@ -85,7 +90,9 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = JUL[JUL['Type']=='INTERNAL']['category']
         task_service = JUL[JUL['Type']=='INTERNAL']['service']
         task_hour = JUL[JUL['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['JUL 30', 'JUl 31'] * int(len(task_service)/2)
+        day_1 = ['JUL 30'] * ((len(task_service)) // 2)
+        day_2 = ['JUL 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     print('JULY TEST DATA:::::::: ', task_category)
 
@@ -95,8 +102,7 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         internal_jobs['Jul']['task service'] = str(task_service.values)
         internal_jobs["Jul"]["hours"] = str(h)
         internal_jobs['Jul']['date'].extend(date_scheduled)
-    else:
-        internal_jobs['Jul'] = 'no jobs scheduled'
+
     
     AUG['Hours'] = [float(i) for i in AUG['Hours']]
     h = (AUG[AUG['Type']=='INTERNAL']['Hours']).sum()
@@ -113,15 +119,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = AUG[AUG['Type']=='INTERNAL']['category']
         task_service = AUG[AUG['Type']=='INTERNAL']['service']
         task_hour = AUG[AUG['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['AUG 30', 'AUG 31'] * int(len(task_service)/2)
+        day_1 = ['AUG 30'] * ((len(task_service)) // 2)
+        day_2 = ['AUG 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Aug"]["task category"].extend(task_category.to_list())
         internal_jobs["Aug"]["task service"].extend(task_service.to_list())
         internal_jobs["Aug"]["hours"].extend(task_hour)
         internal_jobs["Aug"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Aug'] = 'no jobs scheduled'
+
 
     SEP['Hours'] = [float(i) for i in SEP['Hours']]
     h = (SEP[SEP['Type']=='INTERNAL']['Hours']).sum()
@@ -138,15 +145,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = SEP[SEP['Type']=='INTERNAL']['category']
         task_service = SEP[SEP['Type']=='INTERNAL']['service']
         task_hour = SEP[SEP['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['SEP 29', 'SEP 30'] * int(len(task_service)/2)
+        day_1 = ['SEP 29'] * ((len(task_service)) // 2)
+        day_2 = ['SEP 30'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Sep"]["task category"].extend(task_category.to_list())
         internal_jobs["Sep"]["task service"].extend(task_service.to_list())
         internal_jobs["Sep"]["hours"].extend(task_hour)
         internal_jobs["Sep"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Sep'] = 'no jobs scheduled'
+
 
     OCT['Hours'] = [float(i) for i in OCT['Hours']]
     h = (OCT[OCT['Type']=='INTERNAL']['Hours']).sum()
@@ -163,15 +171,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = OCT[OCT['Type']=='INTERNAL']['category']
         task_service = OCT[OCT['Type']=='INTERNAL']['service']
         task_hour = OCT[OCT['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['OCT 30', 'OCT 31'] * int(len(task_service)/2)
+        day_1 = ['OCT 30'] * ((len(task_service)) // 2)
+        day_2 = ['OCT 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Oct"]["task category"].extend(task_category.to_list())
         internal_jobs["Oct"]["task service"].extend(task_service.to_list())
         internal_jobs["Oct"]["hours"].extend(task_hour)
         internal_jobs["Oct"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Oct'] = 'no jobs scheduled'
+
 
 
     NOV['Hours'] = [float(i) for i in NOV['Hours']]
@@ -189,15 +198,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = NOV[NOV['Type']=='INTERNAL']['category']
         task_service = NOV[NOV['Type']=='INTERNAL']['service']
         task_hour = NOV[NOV['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['NOV 29', 'NOV 30'] * int(len(task_service)/2)
+        day_1 = ['NOV 29'] * ((len(task_service)) // 2)
+        day_2 = ['NOV 30'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Nov"]["task category"].extend(task_category.to_list())
         internal_jobs["Nov"]["task service"].extend(task_service.to_list())
         internal_jobs["Nov"]["hours"].extend(task_hour)
         internal_jobs["Nov"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Nov'] = 'no jobs scheduled'
+
 
     DEC['Hours'] = [float(i) for i in DEC['Hours']]
     h = (DEC[DEC['Type']=='INTERNAL']['Hours']).sum()
@@ -214,15 +224,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = DEC[DEC['Type']=='INTERNAL']['category']
         task_service = DEC[DEC['Type']=='INTERNAL']['service']
         task_hour = DEC[DEC['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['DEC 30', 'DEC 31'] * int(len(task_service)/2)
+        day_1 = ['DEC 30'] * ((len(task_service)) // 2)
+        day_2 = ['DEC 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Dec"]["task category"].extend(task_category.to_list())
         internal_jobs["Dec"]["task service"].extend(task_service.to_list())
         internal_jobs["Dec"]["hours"].extend(task_hour)
         internal_jobs["Dec"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Dec'] = 'no jobs scheduled'
+
 
     JAN['Hours'] = [float(i) for i in JAN['Hours']]
     h = (JAN[JAN['Type']=='INTERNAL']['Hours']).sum()
@@ -239,22 +250,21 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = JAN[JAN['Type']=='INTERNAL']['category']
         task_service = JAN[JAN['Type']=='INTERNAL']['service']
         task_hour = JAN[JAN['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['JAN 30', 'JAN 31'] * int(len(task_service)/2)
+        day_1 = ['JAN 30'] * ((len(task_service)) // 2)
+        day_2 = ['JAN 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Jan"]["task category"].extend(task_category.to_list())
         internal_jobs["Jan"]["task service"].extend(task_service.to_list())
         internal_jobs["Jan"]["hours"].extend(task_hour)
         internal_jobs["Jan"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Jan'] = 'no jobs scheduled'
+
 
     FEB['Hours'] = [float(i) for i in FEB['Hours']]
     h = (FEB[FEB['Type']=='INTERNAL']['Hours']).sum()
     
     total_hours += h
-
-
 
     if(h < 8):
         task_category = FEB[FEB['Type']=='INTERNAL']['category']
@@ -266,15 +276,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = FEB[FEB['Type']=='INTERNAL']['category']
         task_service = FEB[FEB['Type']=='INTERNAL']['service']
         task_hour = FEB[FEB['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['FEB 27', 'FEB 28'] * int(len(task_service)/2)
+        day_1 = ['FEB 27'] * ((len(task_service)) // 2)
+        day_2 = ['FEB 28'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Feb"]["task category"].extend(task_category.to_list())
         internal_jobs["Feb"]["task service"].extend(task_service.to_list())
         internal_jobs["Feb"]["hours"].extend(task_hour)
         internal_jobs["Feb"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Feb'] = 'no jobs scheduled'
+
 
     MAR['Hours'] = [float(i) for i in MAR['Hours']]
     h = (MAR[MAR['Type']=='INTERNAL']['Hours']).sum()
@@ -291,15 +302,16 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = MAR[MAR['Type']=='INTERNAL']['category']
         task_service = MAR[MAR['Type']=='INTERNAL']['service']
         task_hour = MAR[MAR['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['MAR 29', 'MAR 30'] * int(len(task_service)/2)
+        day_1 = ['MAR 30'] * ((len(task_service)) // 2)
+        day_2 = ['MAR 31'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Mar"]["task category"].extend(task_category.to_list())
         internal_jobs["Mar"]["task service"].extend(task_service.to_list())
         internal_jobs["Mar"]["hours"].extend(task_hour)
         internal_jobs["Mar"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Mar'] = 'no jobs scheduled'
+  
 
     APR['Hours'] = [float(i) for i in APR['Hours']]
     h = (APR[APR['Type']=='INTERNAL']['Hours']).sum()
@@ -316,16 +328,45 @@ def internal_job_creation(MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, JAN, FEB, MAR,
         task_category = APR[APR['Type']=='INTERNAL']['category']
         task_service = APR[APR['Type']=='INTERNAL']['service']
         task_hour = APR[APR['Type']=='INTERNAL']['Hours']
-        date_scheduled = ['APR 29', 'APR 30'] * int(len(task_service)/2)
+        day_1 = ['APR 29'] * ((len(task_service)) // 2)
+        day_2 = ['APR 30'] * ((len(task_service)) - ((len(task_service)) // 2))
+        date_scheduled = day_1 + day_2
 
     if len(task_category) != 0:
         internal_jobs["Apr"]["task category"].extend(task_category.to_list())
         internal_jobs["Apr"]["task service"].extend(task_service.to_list())
         internal_jobs["Apr"]["hours"].extend(task_hour)
         internal_jobs["Apr"]["date"].extend(date_scheduled)
-    else:
-        internal_jobs['Apr'] = 'no jobs scheduled'
+
+
+    internal_jobs = dict(internal_jobs)
     
+    if 'Landscaping + Lawn Care' in json.dumps(internal_jobs):
+
+        for key, value in internal_jobs.items():
+           
+            value['task category'].extend(['Landscaping + Lawn Care'])
+            value['task service'].extend(['landscaping'])
+            value['hours'].extend([2])
+            value['date'].extend([f'{key} + 30'])
+    
+    if 'Pool' in internal_jobs:
+
+        for key, value in internal_jobs.items():
+           
+            value['task category'].extend(['Pool'])
+            value['task service'].extend(['Pool cleaning & maintenance'])
+            value['hours'].extend([2])
+            value['date'].extend([f'{key} + 30'])
+
+
+    for key, value in internal_jobs.items():
+           
+            value['task category'].extend(['','','',''])
+            value['task service'].extend(['Was work completed or is additional work needed ?','Notice time you spent and any product', 'Anything new discovered or mentioned by member', 'Add pre and post photos'])
+            value['hours'].extend([0, 0, 0, 0])
+            value['date'].extend([f'{key} + 30', f'{key} + 30', f'{key} + 30', f'{key} + 30'])
+
 
     #internal_jobs = {'internal jobs': internal_jobs}
     #internal_jobs = jsonpickle.encode(jsonpickle)
